45b473ec3199bb04786727b947e74a579536974f
Joe Pearson <joe.c.pearson95@gmail.com>
2019-08-22 16:15:02 +0100

    First commit of the project. Learning C# really. Already has Google authentication set up but nothing else has been changed/added, etc. Well, just yet...

---
+++ LearningCSharp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningCSharp.Startup))]
+namespace LearningCSharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
